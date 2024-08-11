119e100d910f13ff39e27c78ff550ac9d2042600
gabrielpontolillo <gabriel.pontolillo@gmail.com>
2024-08-11 18:46:57 +0100

    added mutants for DJ algorithm, beginning to write test execution script for testing

---
+++ case_studies/deutsch_jozsa/mutants/deutsch_jozsa_m6.py
@@ -0,0 +1,17 @@
+from qiskit import QuantumCircuit
+
+
+def deutsch_jozsa_circ(oracle: QuantumCircuit) -> QuantumCircuit:
+    circuit = QuantumCircuit(oracle.num_qubits, oracle.num_qubits)
+    
+    circuit.h(circuit.qubits[-1])  # mutation
+    for qubit in circuit.qubits:
+        circuit.t(qubit)  # mutation
+        
+    circuit.append(oracle, circuit.qubits)
+
+    for qubit in circuit.qubits[:-1]:
+        circuit.h(qubit)
+
+    return circuit
+
